05d533d3e6901ddb9a209af9ea0db09fca151253
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-07-16 11:37:13 +0900

    first commit

---
+++ src/main/java/co/example/GoodCleServerApplication.java
@@ -0,0 +1,13 @@
+package co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodCleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodCleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/example/GoodCleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodCleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
